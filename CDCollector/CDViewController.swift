9817eaa04d1be6392e0c6bd7d14d34b80799a9f9
Jolyon Rudolph <jolyonrudolph@gmail.com>
2019-06-07 11:53:22 +0200

    initial setup

---
+++ CDCollector/CDViewController.swift
@@ -0,0 +1,32 @@
+//
+//  CDViewController.swift
+//  CDCollector
+//
+//  Created by Jolyon Rudolph on 06/06/2019.
+//  Copyright © 2019 Jolyon Rudolph. All rights reserved.
+//
+
+import UIKit
+
+class CDViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func photosTapped(_ sender: Any) {
+    }
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+    }
+}
